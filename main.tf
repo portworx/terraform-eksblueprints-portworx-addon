2195e36298e12038893475167f3689cda13cecac
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-10-10 21:54:29 -0700

    Update main.tf
    Signed-off-by: askulkarni2 <kuapoorv@amazon.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "helm_addon"{
   source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon"
+
   addon_context        = var.addon_context
   set_values           = local.set_values
   helm_config          = local.helm_config
