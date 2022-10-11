8418cae3bd8d62a754dc60fb4830b685ae078b42
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-10-10 21:26:46 -0700

    fix: temporarily unpin module version for terraform 1.3 support

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "helm_addon"{
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.7.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon"
   addon_context        = var.addon_context
   set_values           = local.set_values
   helm_config          = local.helm_config
