7b2f8cd2e6e628d2895641dc6305804d348063f7
Apoorva Kulkarni <askulkarni84@gmail.com>
2022-10-11 00:36:01 -0700

    fix: temporarily unpin module version for terraform 1.3 support (#8)
    * fix: temporarily unpin module version for terraform 1.3 support
    
    * Update main.tf
    
    Signed-off-by: askulkarni2 <kuapoorv@amazon.com>
    
    * fix indent
    
    Signed-off-by: askulkarni2 <kuapoorv@amazon.com>
    
    Signed-off-by: askulkarni2 <kuapoorv@amazon.com>

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "helm_addon"{
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.7.0"
-  addon_context        = var.addon_context
-  set_values           = local.set_values
-  helm_config          = local.helm_config
-  irsa_config          = local.irsa_config
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon"
+
+  addon_context = var.addon_context
+  set_values    = local.set_values
+  helm_config   = local.helm_config
+  irsa_config   = local.irsa_config
 }
