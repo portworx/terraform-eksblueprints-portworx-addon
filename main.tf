27b35863129640466d745b40596c7593413f3d7e
Bryant Biggs <bryantbiggs@gmail.com>
2023-06-05 11:54:44 -0400

    fix: Pin module version used for addon

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "helm_addon" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.32.0"
 
   addon_context = var.addon_context
   helm_config   = local.helm_config
